ff6327f6ab25611c05dcd88a2ee0a479fe76cfd8
frostclaw20 <angel.mercedes20@gmail.com>
2019-02-15 14:42:40 -0500

    Enabled Motion_DC_Motors

--- Main Code/SDC_Bot_Arduino/SDC_Bot_Arduino.ino
+++ Main Code/SDC_Bot_Arduino/SDC_Bot_Arduino.ino
@@ -7,7 +7,7 @@
 //#include "IMU_Data.h"                 //Information adquired from the innertial measurement unit
 //#include "Picking_Steppers.h"         //Motion for the stepper motors used for the picking mechanism
 //#include "Door_Shell_DC_Motor.h"      //Motion for the DC motors used for the door and shell mechanism
-//#include "Motion_DC_Motors.h"         //Motion for the DC motors used for the mecanum wheels to move the body
+#include "Motion_DC_Motors.h"         //Motion for the DC motors used for the mecanum wheels to move the body
 #include "Piezo_Sound.h"              //Signal sent to the piezo speaker
 //#include "Communications.h"           //Communications for sending and recieving information
 #include "RC_Comms.h"
