470e475938a27c5ee195403e232d07992a1086a7
frostclaw20 <angel.mercedes20@gmail.com>
2019-03-26 14:48:02 -0400

    changes and cleanup of the pinout in order to make it cleaner

---
+++ Example & Test Code/ServoTestCode/ServoTestCode.ino
@@ -0,0 +1,44 @@
+/*This is the code that is used for the two servos that open and close the gates.
+ * 
+ * Pinout:
+ * ServoLeft = 
+ * ServoRight = 
+ * Both pins are PWM enabled pins
+ */
+
+#define ServoPinLeft
+#define ServoPinRight 
+
+int OpenPosition = 0;
+int ClosedPosition = 0;
+
+void ServoSetup(){
+
+  
+}
+
+void GateOpen(){
+
+  
+}
+
+void GateClose(){
+
+
+  
+}
+void ServoCalibration(int TestAngleServo){
+
+
+  
+}
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
