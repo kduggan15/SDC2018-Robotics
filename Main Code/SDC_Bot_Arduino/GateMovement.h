78dffda35dc1551e5a7586123c693ab3cb2ef37c
Jorge Roldan-Roldan <jorgeroldanroldan@gmail.com>
2019-04-02 21:37:44 -0400

    Reasigned servo pin

--- Main Code/SDC_Bot_Arduino/GateMovement.h
+++ Main Code/SDC_Bot_Arduino/GateMovement.h
@@ -1,43 +1,43 @@
 /*This is the code that is used for the two servos that open and close the gates.
- * 
+ *
  * Pinout:
  * ServoLeft = 12
  * ServoRight = 13
  * Both pins are PWM enabled pins
  */
 
- /*Note 1: 
-  * In order for servos to move to a specific position, you need to send it an pwm value that will send it it's positon. 
-  * Our problem is the following, the two servo motors are placed in opposite directions, as one moves clockwise, the other will move counterclockwise. 
-  * The problem here is that servos consider their 0 degrees position is relative to the clockwise direction, the higher you in degrees, the more you move clockwise. 
-  * Why is this a problem? because one of the motors is opposite, instead of for instance, going to 30 degrees as commanded, it will be basically be going to 150 degrees. 
+ /*Note 1:
+  * In order for servos to move to a specific position, you need to send it an pwm value that will send it it's positon.
+  * Our problem is the following, the two servo motors are placed in opposite directions, as one moves clockwise, the other will move counterclockwise.
+  * The problem here is that servos consider their 0 degrees position is relative to the clockwise direction, the higher you in degrees, the more you move clockwise.
+  * Why is this a problem? because one of the motors is opposite, instead of for instance, going to 30 degrees as commanded, it will be basically be going to 150 degrees.
   * To correct this, we inverse the values being sent to one of the motors, so instead of sending it 30, we send it 150, which will turn the motor to the 30 position lol
-  * that is what the function does. 
+  * that is what the function does.
   */
 #ifndef GATE_MOVEMENT_H
 #define GATE_MOVEMENT_H
-#include <Servo.h> 
+#include <Servo.h>
 
 #define ServoPinLeft 12   //Pin used for the left servo signal
-#define ServoPinRight 13  //Pin used for the right servo signal
+#define ServoPinRight 40  //Pin used for the right servo signal
 
 Servo ServoLeft;          //Object initialization for the left servo
 Servo ServoRight;         //Object initialization for the right servo
 
-//Position values for opening and closing the door, these need to be calibrated before being set. 
+//Position values for opening and closing the door, these need to be calibrated before being set.
 
 const int OpenPosition = 180;     //Value in degrees at which the gate will swing open
 const int ClosedPosition = 20;   //Value in degrees at which the gate will swing closed
-int GateStatus = 0;             //Flag that stores position of the gate, 0 = No position assigned, 1 = Open, 2 = closed. 
+int GateStatus = 0;             //Flag that stores position of the gate, 0 = No position assigned, 1 = Open, 2 = closed.
 
 //Setup for the servos, add this to the main setup.
 void ServoSetup(){
-    //Initialization of servos based on the library setup, just gives the object, the pin it needs to use. 
+    //Initialization of servos based on the library setup, just gives the object, the pin it needs to use.
     ServoLeft.attach(12); //initialization Left servo
-    ServoRight.attach(13); //Initialization Right Servo. 
+    ServoRight.attach(13); //Initialization Right Servo.
 }
 
-//Opens the gate, using the values given by the constant position values of position. 
+//Opens the gate, using the values given by the constant position values of position.
 void GateOpen(){
 
   ServoLeft.write(OpenPosition);                        //Sends position information to left servo
@@ -48,37 +48,37 @@ void GateOpen(){
   if(SystemDebug == 3){
     Serial.print("Gate Status:"); //Status printing
     Serial.println("Open");
-    
+
     Serial.print(" Position: "); //Position printing
     Serial.print(OpenPosition);
     Serial.print(" Degrees");
   }
 }
 
-//Closes the gate, using the values given by the constant position values of position. 
+//Closes the gate, using the values given by the constant position values of position.
 void GateClose(){
 
   ServoLeft.write(ClosedPosition);                        //Sends position information to left servo
-  ServoRight.write(ClosedPosition);  //Sends posiition information to right servo 
+  ServoRight.write(ClosedPosition);  //Sends posiition information to right servo
   GateStatus = 2; //Gate closed
 
   //Prints the status of the gate and position value if debugging is enabled
   if(SystemDebug == 3){
     Serial.print("Gate Status:"); //Status Printing
     Serial.print("Closed");
-    
+
     Serial.print(" Position: "); //Position Printing
     Serial.print(ClosedPosition);
     Serial.print(" Degrees");
-  } 
+  }
 }
 
 /*
 This is a function that will allow you to move the servos to a specified angle. Call the function and give it the angle
 you would like both servos to move to. This will allow you to choose the angle at which the servos will open and close
-the gate. 
+the gate.
 
-This would be kind of hard to do manually with the controller, so just changing the function value in the code and reflashing 
+This would be kind of hard to do manually with the controller, so just changing the function value in the code and reflashing
 should be fine. ;-)
 */
 
